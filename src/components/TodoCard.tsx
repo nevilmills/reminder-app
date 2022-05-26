70cf1da47686c8f83a9fa0fde12f23c393deb5eb
Nevil <nevilmills@gmail.com>
2022-05-26 10:38:52 -0700

    Initial commit

---
+++ src/components/TodoCard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface TodoCardProps {}
+
+export const TodoCard: React.FC<TodoCardProps> = ({}) => {
+  return (
+    <div className="w-[800px] h-[500px] bg-gray-500 border-2 border-gray-600">
+      <div>yo</div>
+    </div>
+  );
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div className="h-screen w-screen bg-[#f1f1f1] flex items-center justify-center">
+      <div></div>
+    </div>
+  );
+};
+
+export default Home;
